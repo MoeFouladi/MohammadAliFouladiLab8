de6c91575e65986fd5e867dc14d3a1c8d8661b3a
origin <mh.fouladi@hotmail.com>
2024-07-10 19:58:19 -0400

    added settings with image from phone

--- MohammadAliModule8/src/main/java/mohammadali/fouladi/n01547173/SettingsFragment.java
+++ MohammadAliModule8/src/main/java/mohammadali/fouladi/n01547173/SettingsFragment.java
@@ -1,12 +1,24 @@
 package mohammadali.fouladi.n01547173;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +35,32 @@ public class SettingsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private ImageView imageView;
+    private Button button;
+    private int count = 0;
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                    Toast.makeText(getActivity(), R.string.permission_allowed, Toast.LENGTH_SHORT).show();
+                    pickImageFromGallery();
+                } else {
+                    Toast.makeText(getActivity(), R.string.permission_denied, Toast.LENGTH_SHORT).show();
+                    showSettingsDialog();
+                }
+            });
+
+    private final ActivityResultLauncher<Intent> pickImageLauncher =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+                if (result.getResultCode() == getActivity().RESULT_OK) {
+                    if (result.getData() != null) {
+                        Uri selectedImageUri = result.getData().getData();
+                        imageView.setImageURI(selectedImageUri);
+                    }
+                }
+            });
+
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -54,11 +92,48 @@ public class SettingsFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        button = view.findViewById(R.id.Settingsbutton);
+        imageView= view.findViewById(R.id.SettingsimageView);
+        button.setOnClickListener(v ->  {
+            if (ActivityCompat.checkSelfPermission(this.requireContext(), Manifest.permission.READ_MEDIA_IMAGES )
+                    != PackageManager.PERMISSION_GRANTED) {
+                count++;
+                if (count == 3) {
+                    showSettingsDialog();
+                } else {
+                    requestPermissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES);
+                }
+            } else {
+                pickImageFromGallery();
+            }
+
+        });
+
+        return view;
+    }
+
+
+    private void showSettingsDialog() {
+        new androidx.appcompat.app.AlertDialog.Builder(this.requireContext())
+                .setTitle("Permission Denied")
+                .setMessage("You have denied the permission. Please enable it in app settings.")
+                .setPositiveButton("Go to Settings", (dialog, which) -> {
+                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts("package", this.requireContext().getPackageName(), null);
+                    intent.setData(uri);
+                    startActivity(intent);
+                })
+                .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss())
+                .create().show();
+    }
+    private void pickImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        pickImageLauncher.launch(intent);
     }
 }

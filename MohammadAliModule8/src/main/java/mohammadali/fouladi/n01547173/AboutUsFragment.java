599e017ae87c64d815678f85deb1f1c71b3a490d
origin <mh.fouladi@hotmail.com>
2024-07-11 10:48:33 -0400

    added about us screen

--- MohammadAliModule8/src/main/java/mohammadali/fouladi/n01547173/AboutUsFragment.java
+++ MohammadAliModule8/src/main/java/mohammadali/fouladi/n01547173/AboutUsFragment.java
@@ -1,5 +1,8 @@
 package mohammadali.fouladi.n01547173;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +10,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,10 +25,14 @@ public class AboutUsFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String SHARED_PREFS_NAME = "profile_prefs";
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private SharedPreferences sharedPreferences;
+    private int count;
 
     public AboutUsFragment() {
         // Required empty public constructor
@@ -49,16 +59,44 @@ public class AboutUsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        sharedPreferences = getActivity().getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE);
+
+        count = 0;
+        count++;
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.apply();
+
+        Toast.makeText(getActivity(), getString(R.string.access_counter) + count + "\n"+getString(R.string.Fullname), Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_us, container, false);
+     View view = inflater.inflate(R.layout.fragment_about_us, container, false);
+        TextView sharedPrefsTextView = view.findViewById(R.id.sharedTV);
+        boolean checkboxChecked = sharedPreferences.getBoolean("key_checkbox", false);
+        String email = sharedPreferences.getString("key_email", getString(R.string.no_data));
+        String id = sharedPreferences.getString("key_id", getString(R.string.no_data));
+        sharedPrefsTextView.setText(getString(R.string.checkbox)+": " + checkboxChecked + "\nEmail: " + email + "\nID: " + id);
+
+        ToggleButton toggleButton = view.findViewById(R.id.toggleButton);
+        toggleButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            } else {
+                getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+            }
+        });
+
+        return view;
+
     }
 }
